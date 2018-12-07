ec558486dfc85896f4cfc21f912b1a82271b1770
hao.hung <hao.hung@hy-tech.com.tw>
2018-12-07 13:19:30 +0800

    載入外部套件 自定樣式

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import VeeValidate, { Validator } from 'vee-validate';
+import zhTwValidate from 'vee-validate/dist/locale/zh_TW';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(VueAxios, axios);
+Vue.use(VeeValidate);
+Validator.localize('zhTw', zhTwValidate);
+Vue.component('Loading', Loading);
 Vue.config.productionTip = false;
+axios.defaults.withCredentials = true;
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app');
+
+router.beforeEach((to, from, next) => {
+  // console.log('to', to, 'from', from, 'next', next);
+  if (to.meta.requiresAuth) {
+    const path = `${process.env.VUE_APP_APIPATH}/api/user/check`;
+    axios.post(path).then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        next();
+      } else {
+        next({
+          path: '/login',
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
